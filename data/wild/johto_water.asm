02c54d70442470305ef947ac01c4a5d0231bc010
PiaCarrot <datingsimsrs@gmail.com>
2023-05-11 16:34:22 -0400

    Day/Night Water Encounters

--- data/wild/johto_water.asm
+++ data/wild/johto_water.asm
@@ -3,7 +3,24 @@
 JohtoWaterWildMons:
 
 	def_water_wildmons VALENCIA_ISLAND
-	db 100 percent ; encounter rate
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 2, TENTACOOL
+	dbw 2, WINGULL
+	dbw 3, WINGULL
+	dbw 3, WINGULL
+	dbw 2, HOPPIP
+	dbw 3, HOPPIP
+	dbw 3, HOPPIP
+	; day
+	dbw 2, TENTACOOL
+	dbw 2, WINGULL
+	dbw 3, WINGULL
+	dbw 3, WINGULL
+	dbw 2, HOPPIP
+	dbw 3, HOPPIP
+	dbw 3, HOPPIP
+	; nite
 	dbw 2, TENTACOOL
 	dbw 2, WINGULL
 	dbw 3, WINGULL
