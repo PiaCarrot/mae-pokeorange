7a2ec42bee002989a1e864a43bf51ad27776407a
8bitZeta <devin94@protonmail.com>
2023-08-08 11:13:25 -0500

    Fix Rollout Animation

--- maps/basegame/PlayersHouse2F.asm
+++ maps/basegame/PlayersHouse2F.asm
@@ -129,7 +129,7 @@ if DEF(_DEBUG)
 	; setflag ENGINE_FLYPOINT_CINNABAR
 	; test mons
 	givepoke MEW, 100, LEFTOVERS
-	givepokemove TRICK_ROOM,        wPartyMon1, 0
+	givepokemove ROLLOUT,        wPartyMon1, 0
 	givepokemove DRACO_METEOR,       wPartyMon1, 1
 	givepokemove PUNISHMENT,   wPartyMon1, 2
 	givepokemove WEATHER_BALL,        wPartyMon1, 3
